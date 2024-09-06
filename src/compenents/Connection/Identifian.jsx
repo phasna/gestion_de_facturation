8a10a2f032f6845522ace15fe75ea2cc822b113f
Ziad FAIH <ziad.faih2002@gmail.com>
2024-09-06 15:07:26 +0200

    identifiant

--- src/compenents/Connection/Identifian.jsx
+++ src/compenents/Connection/Identifian.jsx
@@ -5,8 +5,8 @@ import React from 'react';
 const Identifian = () => {
     return (
 
-        <form className="bg-gray-200 rounded-lg w-2/4 h-auto p-4 flex justify-center items-center">
-            <div className="">
+        <form className="bg-black rounded-lg w-2/4 h-auto p-4 flex justify-center items-center  ">
+            <div className="bg-gray-200 rounded-lg w-3/4 h-auto p-4 ">
                 <h1 className="flex justify-center pb-2.5">Connectez-vous !</h1>
                 <div className="group">
                     <label htmlFor="zip" className="rounded-full py-3 px-6...">Identifiant</label>
@@ -14,7 +14,7 @@ const Identifian = () => {
                         type="text"
                         id="zip"
                         name="zip"
-                        className="mt-1 block  px-10 py-2  border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                        className="mt-1 block  w-full py-2  border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
 
                         required
                     />
@@ -24,10 +24,11 @@ const Identifian = () => {
                     <input type="text"
                            id="zip"
                            name="zip"
-                           className="mt-1 block  px-10 py-2  border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                           className="mt-1 block  w-full py-2  border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
 
                            required/>
                 </div>
+                <a>Mot de passe oublié ?</a>
                 <div className="group">
                     <button type="button"
                             className="mt-4 px-4 py-2 bg-black text-white font-semibold rounded-md shadow-sm  focus:outline-none">Connecter
--- src/compenents/Connection/mdpoublier.jsx
+++ src/compenents/Connection/mdpoublier.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Mdpoublier = () => {
     return (
-        <form className="flex justify-center items-center">
-            <div className="bg-gray-200 rounded-lg w-2/4 h-auto p-4 ">
+        <form className=" bg-black rounded-lg w-2/4 h-auto p-4 flex justify-center items-center  ">
+            <div className="bg-gray-200 rounded-lg w-3/4 h-auto p-4 ">
                 <h1 className="flex justify-center pb-2.5">Connectez-vous !</h1>
                 <div className="group">
                     <label htmlFor="zip" className="rounded-full py-3 px-6...">Votre email !</label>
@@ -11,7 +11,7 @@ const Mdpoublier = () => {
                         type="text"
                         id="zip"
                         name="zip"
-                        className="mt-1 block  px-10 py-2  border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                        className="mt-1 block w-full  px-10 py-2  border border-gray-300  rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
 
                         required
                     />
