2e5f4bad4634de6d223aa61cf769775da24eaf3f
phasna cruz <phasna@mbp-de-phasna.home>
2024-10-09 13:32:32 +0200

    phasna

--- src/compenents/campany/AccueilImage.jsx
+++ src/compenents/campany/AccueilImage.jsx
@@ -1,29 +1,29 @@
-import Image from "../../assets/img/Image_02.png";
+import Image from "../../assets/img/Image_03.png";
 
 const AccueilImage = () => {
     return (
-        <div className="bg-gradient-to-r from-blue-800 to-blue-700 w-full h-72 p-10 rounded-lg shadow-lg flex justify-between items-center">
+        <div className="bg-gradient-to-r from-blue-800 to-blue-700 w-full h-96 p-10 rounded-lg shadow-lg flex justify-between items-center">
             {/* Section texte */}
-            <div className="w-1/2 pr-10">
-                <h1 className="text-4xl mb-6 text-white font-extrabold leading-tight">
+            <div className="w-1/2 space-y-6 pr-10 p-10">
+                <h1 className="text-4xl text-white font-extrabold leading-tight">
                     Bienvenue dans l'interface
                 </h1>
-                <p className="text-white mb-6 text-lg leading-relaxed">
+                <p className="text-white text-lg leading-relaxed">
                     Créez et ajoutez des factures & des clients facilement.
                 </p>
-
-                <button className="px-8 py-4 rounded-lg bg-green-500 hover:bg-green-600 text-white font-semibold">
+                <button className="px-8 py-4 rounded-lg bg-green-500 hover:bg-green-600 text-white font-semibold transition-colors duration-300">
                     Ajouter un client
                 </button>
             </div>
 
             {/* Section image */}
-            <div className="w-1/2 h-full flex items-center justify-center">
-                <div className="h-full w-1/3 rounded-lg overflow-hidden shadow-md">
+            <div className="w-1/2 flex items-center justify-center">
+                <div className="h-full w-96 rounded-lg overflow-hidden">
                     <img
                         src={Image}
                         alt="Illustration"
                         className="w-full h-full object-cover"
+                        loading="lazy" // Améliore les performances en retardant le chargement de l'image
                     />
                 </div>
             </div>
